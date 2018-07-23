a7c227b9a77e6667efa3a6a9ec827c8fd085cadb
François Georgy <francois.georgy@gmail.com>
2018-07-23 17:28:48 +0200

    drawing the fretboard (incomplete)

--- src/component/Fretboard.js
+++ src/component/Fretboard.js
@@ -11,9 +11,28 @@ const defaultProps = {
     frets: 5
 };
 
+function getStringsPath(strings, frets) {
+
+    // could be simplified with the syntax Array.apply(null, Array(N)).map(...)
+
+    let s = new Array(strings);
+    for (let i=0; i<strings; i++) {
+        s[i] = `M 0,${i} H 10`
+    }
+    return s.join(' ');
+}
+
+function getFretsPath(strings, frets) {
+    return '';
+}
+
 export default class Fretboard extends React.Component {
 
     render() {
+
+        let s = getStringsPath(this.props.strings, this.props.frets);
+        console.log(s);
+
         return <div>fretboard with {this.props.strings} strings and {this.props.frets} frets.</div>
     }
 
