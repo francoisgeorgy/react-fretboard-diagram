1934a4cfd53d717a87165b1e5afda6566dbe9d10
François Georgy <francois.georgy@gmail.com>
2018-08-21 15:49:22 +0200

    use fretboard-api

---
+++ src/sample-app/FretboardApi.js
@@ -0,0 +1,14 @@
+import {Component} from "react";
+
+class FretboardApi extends Component {
+
+    render() {
+
+        // let shape = new Shape("022100");
+
+        return (
+            <div></div>
+        );
+    }
+
+}
